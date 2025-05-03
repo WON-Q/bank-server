f0c4ad2eabbf29b41e2e504f1414bd5fcb26386a
yxhwxn <officeyh9819@gmail.com>
2025-05-03 18:29:53 +0900

    클라이언트 테스트용 최소 HTTP 응답 추가

--- src/http.c
+++ src/http.c
@@ -2,9 +2,22 @@
 #include "http.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>  // strlen
 
 void handle_connection(int client_fd) {
-    // TODO: read(), 파싱, account_deposit/withdraw 호출, write(), close()
-    printf("Accepted fd=%d\n", client_fd);
+    // 간단한 OK 응답
+    const char *body = "OK";
+    char header[128];
+    int header_len = snprintf(header, sizeof(header),
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Length: %zu\r\n"
+        "Content-Type: text/plain\r\n"
+        "\r\n",
+        strlen(body)
+    );
+    write(client_fd, header, header_len);
+    write(client_fd, body, strlen(body));
+
     close(client_fd);
 }
+
