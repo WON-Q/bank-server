a6d5be1c8806ad929a058a881026ad56a0fc2fce
yxhwxn <officeyh9819@gmail.com>
2025-05-03 18:18:49 +0900

    main.c 수정: 스레드풀 초기화, accept() 시스템콜 호출 루프 추가

--- src/main.c
+++ src/main.c
@@ -1,10 +1,45 @@
 // src/main.c
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
 #include "account.h"
+#include "threadpool.h"
+
+#define PORT 9090
+
+static int listen_fd;
+
+void handle_sigint(int signo) {
+    (void)signo;
+    printf("\nShutting down...\n");
+    close(listen_fd);
+    exit(0);
+}
 
 int main(void) {
+    signal(SIGINT, handle_sigint);
+
     account_module_init();
-    printf("Bank server starting on port 9090...\n");
+    threadpool_init();
+
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_addr.s_addr = INADDR_ANY,
+        .sin_port = htons(PORT)
+    };
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(listen_fd, (struct sockaddr*)&addr, sizeof(addr));
+    listen(listen_fd, 10);
+    printf("Bank server listening on port %d...\n", PORT);
+
+    while (1) {
+        int client_fd = accept(listen_fd, NULL, NULL);
+        threadpool_add_task(client_fd);
+    }
+
     return 0;
 }
 
