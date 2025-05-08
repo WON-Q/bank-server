d962e0a22a5c2482493ef2109df166ffe315d4b3
yxhwxn <officeyh9819@gmail.com>
2025-05-08 12:23:30 +0900

    [feat/3]: 입금 트랜잭션 구현

--- src/db.c
+++ src/db.c
@@ -21,3 +21,49 @@ bool db_init(const char *host, const char *user,
     return true;
 }
 
+int db_deposit(int id, long amount, long *new_balance) {
+    MYSQL_STMT *stmt;
+    MYSQL_BIND bind[2];
+    long bal;
+
+    mysql_query(conn, "START TRANSACTION");
+
+    // 1) SELECT balance FOR UPDATE
+    stmt = mysql_stmt_init(conn);
+    mysql_stmt_prepare(stmt,
+      "SELECT balance FROM accounts WHERE id = ? FOR UPDATE", -1);
+    // bind id
+    bind[0].buffer_type = MYSQL_TYPE_LONG;
+    bind[0].buffer      = &id;
+    mysql_stmt_bind_param(stmt, bind);
+    mysql_stmt_execute(stmt);
+    mysql_stmt_bind_result(stmt, bind);
+    if (mysql_stmt_fetch(stmt) != 0) {
+        mysql_stmt_close(stmt);
+        mysql_query(conn, "ROLLBACK");
+        return -2;  // 계좌 없음
+    }
+    bal = *(long*)bind[0].buffer;
+    mysql_stmt_close(stmt);
+
+    // 2) UPDATE accounts SET balance = ? WHERE id = ?
+    bal += amount;
+    stmt = mysql_stmt_init(conn);
+    mysql_stmt_prepare(stmt,
+      "UPDATE accounts SET balance = ? WHERE id = ?", -1);
+    memset(bind, 0, sizeof(bind));
+    bind[0].buffer_type = MYSQL_TYPE_LONGLONG;
+    bind[0].buffer      = &bal;
+    bind[1].buffer_type = MYSQL_TYPE_LONG;
+    bind[1].buffer      = &id;
+    mysql_stmt_bind_param(stmt, bind);
+    mysql_stmt_execute(stmt);
+    mysql_stmt_close(stmt);
+
+    // 3) COMMIT
+    mysql_query(conn, "COMMIT");
+
+    *new_balance = bal;
+    return 0;
+}
+
