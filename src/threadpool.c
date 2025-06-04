6908e3bc5a5338db3828a1b4eb7bc1962ae30509
yxhwxn <officeyh9819@gmail.com>
2025-06-04 21:39:02 +0900

    Chore: DB source 수정

--- src/threadpool.c
+++ src/threadpool.c
@@ -24,7 +24,7 @@ static void *worker_loop(void *arg) {
     // — 워커 스레드 전용 MySQL 초기화 —
     mysql_thread_init();
     if (!db_init(
-            "34.47.89.238",   // MySQL 호스트
+            "34.22.64.79",   // MySQL 호스트
             "root",        // 사용자
             "12345",  // 비밀번호
             "bank",        // DB명
